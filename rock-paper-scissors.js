8176173746ced58696b0b247b3bbd5e2483c33a2
christopher <254568270+CBxyliss@users.noreply.github.com>
2026-04-13 15:07:48 +0100

    Add number generator and computer choice function
    Set up a random number generator to work with existing computer choice
    function and future player choice function, computer choice function is
    returning correct values

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,23 @@
-console.log("Hello World");
+/* Create the game rock, paper, scissors */
+
+/* Write the logic to get computer's choice */
+
+/* Generate number from 1 to 3 */
+const min = 1
+const max = 3
+
+const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min
+
+/* Use if...else statements to get computer choice based on number generated */
+
+function getComputerChoice() {
+    if (randomNumber === 1) {
+        return "rock"
+    } else if (randomNumber === 2) {
+        return "paper"
+    } else if (randomNumber === 3) {
+        return "scissors"
+    }
+} 
+
+console.log(getComputerChoice());
